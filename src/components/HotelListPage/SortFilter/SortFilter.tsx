de2989da418327d37c6cd1f89a1ab7542d373f2e
gg05t <144322678+gg05t@users.noreply.github.com>
2024-04-13 23:07:03 +0700

    add img

--- src/components/HotelListPage/SortFilter/SortFilter.tsx
+++ src/components/HotelListPage/SortFilter/SortFilter.tsx
@@ -1,19 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+
 const SortItem = [
    {
     name: "Price",
-    img: "/",
+    img: "/sortingIcon/priceIcon.png",
   },
   {
     name: "Rating",
-    img: "/",
+    img: "/sortingIcon/ratingIcon.png",
   },
   {
     name: "Distance",
-    img: "/",
+    img: "/sortingIcon/distanceIcon.png",
   },
   {
     name: "Name",
-    img: "/",
+    img: "/sortingIcon/nameIcon.png",
   },
 ];
 
@@ -22,7 +25,9 @@ export default function SortFilter() {
     <div className=" mx-auto flex mt-1 text-sm text-[#15439C] rounded-md border-2 border-[#15439C] items-center font-bold">
       <div className="flex px-3 space-x-2 ">
         <p>Sort By</p>
-        <div className="h-6 w-6 bg-zinc-300"></div>
+        <div className="h-6 w-6 bg-zinc-300"> 
+        <Image src={"/sortingIcon/sortByIcon.png"} alt="Sort By Icon" width={24} height={24} />
+      </div>
       </div>
       {SortItem.map((item, index) => {
         return (
@@ -31,7 +36,9 @@ export default function SortFilter() {
             className="flex border-l-2 border-black px-5 py-1  space-x-2 hover:bg-slate-100 "
           >
             <p>{item.name}</p>
-            <div className="h-6 w-6 bg-zinc-300"></div>
+            <div className="h-6 w-6 ">
+            <Image src={item.img} alt={`${item.name} Icon`} width={200} height={200} />
+            </div>
           </button>
         );
       })}
