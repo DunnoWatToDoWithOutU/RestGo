89bbfa9f298b5ef8fea782216a4b863f8aa97720
understann <nattapon.how@gmail.com>
2024-04-13 22:30:18 +0700

    delete box at the ending of sort by

--- src/components/HotelListPage/SortFilter/SortFilter.tsx
+++ src/components/HotelListPage/SortFilter/SortFilter.tsx
@@ -16,9 +16,8 @@ const SortItem = [
 export default function SortFilter() {
   return (
     <div className=" mx-auto flex mt-1 text-sm text-[#15439C] rounded-md border-2 border-[#15439C] items-center font-bold">
-      <div className="flex px-3      space-x-2 ">
+      <div className="flex px-3 space-x-2 ">
         <p>Sort By</p>
-        <div className="h-6 w-6 bg-zinc-300"></div>
       </div>
       {SortItem.map((item, index) => {
         return (
