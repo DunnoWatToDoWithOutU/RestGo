104a389b7add9c02c72f7ff37569691b1701eec3
Nanfa Khuntawee <khuntawee.nanfa@gmail.com>
2024-04-13 21:07:41 +0700

    fix url

--- src/libs/login.ts
+++ src/libs/login.ts
@@ -1,5 +1,5 @@
 export async function login(email: string, password: string) {
-    const response = await fetch(`http://localhost:3000/api/v1/auth/login`,{
+    const response = await fetch(`https://rest-go.vercel.app/api/v1/auth/login`,{
         method: "POST",
         headers: {
             "Content-Type": "application/json"
