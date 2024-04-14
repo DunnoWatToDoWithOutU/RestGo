8a3a0bcf56a8100650a8578adabac5f29896ffea
Sigazen <choikdee50@gmail.com>
2024-04-14 19:50:01 +0700

    add banner

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,17 +2,29 @@ import getHotels from "@/libs/getHotels";
 import { HotelList } from "./HotelList";
 import { MenuBox } from "./MenuBox/MenuBox";
 import { SearchBar } from "./SearchBar";
+import Image from "next/image";
 
 export async function HomePage() {
   const hotels = await getHotels();
   return (
-    <div className="px-[10%] mt-20 text-center ">
-      <p className=" text-3xl font-semibold">
-        Welcome to REST GO. We will get you to rest in piece.
-      </p>
-      <SearchBar></SearchBar>
-      <MenuBox></MenuBox>
-      <HotelList HotelData={hotels}></HotelList>
+    <div>
+      <div className="px-[10%] mt-20  relative z-10 text-center ">
+        <p className=" text-3xl font-semibold text-white ">
+          Welcome to REST GO. We will get you to rest in piece.
+        </p>
+        <SearchBar></SearchBar>
+        <MenuBox></MenuBox>
+        <HotelList HotelData={hotels}></HotelList>
+      </div>
+      <div className=" overflow-hidden h-96 w-full bg-black z-0 absolute left-0 top-0">
+        <Image
+          src={"/img/homepage.png"}
+          alt="hotelhomepage"
+          objectFit="cover"
+          layout="fill"
+          className=" opacity-[0.87]"
+        ></Image>
+      </div>
     </div>
   );
 }
