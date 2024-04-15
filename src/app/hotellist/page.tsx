481dd75715de939a2bbce9da19be80af8720cef2
Alknot1122 <117248001+Alknot1122@users.noreply.github.com>
2024-04-15 16:55:16 +0700

    Update HotelList to use SearchBar from SearchBar Component

--- src/app/hotellist/page.tsx
+++ src/app/hotellist/page.tsx
@@ -3,6 +3,7 @@ import { TagContainer } from "@/components/HotelListPage/TagFilter/TagContainer"
 import SortFilter from "@/components/HotelListPage/SortFilter/SortFilter";
 import { HotelList } from "@/components/HomePage/HotelList";
 import getHotels from "@/libs/getHotels";
+import { SearchBar } from "@/components/HomePage/SearchBar";
 
 export default async function HotelListPage() {
   const hotels = await getHotels();
@@ -10,23 +11,9 @@ export default async function HotelListPage() {
     <main className="w-full flex flex-col justify-start items-center px-[10%]">
       <div className="relative  w-[40%] mt-10 text-center">
         <p className=" text-3xl font-bold mb-5 text-[#15439C]">Hotel List</p>
-        <div className="flex justify-between border-2 rounded-full px-3 pr-5 border-blue-800 items-center  focus:outline-none">
-          <input
-            type="text"
-            id="password"
-            className="px-5 py-1  w-full focus:outline-none "
-            placeholder="Search..."
-          ></input>
-          <button className="py-2">
-            {" "}
-            <Image
-              src={`/icon/searchIcon.png`}
-              alt="search Icon"
-              width={20}
-              height={20}
-            ></Image>
-          </button>
-        </div>
+        <div className="flex justify-between items-center">
+          <SearchBar></SearchBar>
+          </div>
       </div>
       <TagContainer></TagContainer>
       <SortFilter></SortFilter>
