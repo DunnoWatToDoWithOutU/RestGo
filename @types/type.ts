b7c4514892ec049c5e53c2f4d7cb6882d1cd76ed
Sigazen <choikdee50@gmail.com>
2024-04-16 17:26:50 +0700

    add Tag for hotel Card

--- @types/type.ts
+++ @types/type.ts
@@ -1,10 +1,12 @@
+
+
 export interface HotelProps{
     id:string;
     name:string;
     address:string,
     telephone:string,
     price:number,
-    tags:string[],
+    tag:string[],
     reviews:string[],
     pic:string[],
 }
