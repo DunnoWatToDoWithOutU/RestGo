6c5bb3f6a15401b36860d3551d5266a95e799d16
Sigazen <choikdee50@gmail.com>
2024-04-19 00:26:43 +0700

    update expire appointment

--- src/app/api/v1/appointments/route.ts
+++ src/app/api/v1/appointments/route.ts
@@ -4,16 +4,17 @@ import connectDB from "@/libs/connectDB";
 import protect from "@/libs/protect";
 export async function GET(req: NextRequest) {
     const user = await protect(req);
+    console.log(user);
     if (!user) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
     try { 
         await connectDB();
         if( user.role !== "admin" ){
-            const appointments = await Appointment.find({ user:user._id });
+            const appointments = await Appointment.find({ user:user._id , endDate: { $gte: new Date() }});
             return NextResponse.json(appointments, { status: 200 });
         } else {
-            const appointments = await Appointment.find();
+            const appointments = await Appointment.find({endDate: { $gte: new Date() }});
             return NextResponse.json(appointments, { status: 200 });
         }
     } catch (err) {
