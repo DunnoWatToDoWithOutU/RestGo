4788877cc310149ef5f08e8e2347fee6527d514c
tewpongamorn123 <tewpongamorn@gmail.com>
2024-04-18 16:42:42 +0700

    add Promos deletion

--- src/components/Promotion/PromotionData.tsx
+++ src/components/Promotion/PromotionData.tsx
@@ -1,9 +1,20 @@
-import getPromotions from "@/libs/getPromotions";
-import ProButton from "./ProButton";
-import { PromotionProps } from "../../../@types/type";
-import getHotel from "@/libs/getHotel";
+import React, { useState, useEffect } from 'react';
+import ProButton from './ProButton';
+import { PromotionProps } from '../../../@types/type';
+import getPromotions from '@/libs/getPromotions';
 
-export default async function PromotionData() {
-  const promotion: PromotionProps[] = await getPromotions();
-  return <ProButton promotion={promotion}></ProButton>;
+export default function PromotionData() {
+    const [promotions, setPromotions] = useState<PromotionProps[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const promotionData: PromotionProps[] = await getPromotions();
+                setPromotions(promotionData);
+            } catch (error) {
+                console.error('Error fetching promotions:', error);
+            }
+        };
+        fetchData();
+    }, []);
+    return <ProButton promotion={promotions}></ProButton>;
 }
---
+++ src/libs/deletePromotion.tsx
@@ -0,0 +1,14 @@
+export default async function deletePromotion(promotionId: string) {
+    const response = await fetch(`https://rest-go.vercel.app/api/v1/promotions/${promotionId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: "include"
+    });
+    if (!response.ok) {
+        throw new Error("Error failed to delete promotion");
+    }
+    const data = await response.json();
+    return data;
+}
