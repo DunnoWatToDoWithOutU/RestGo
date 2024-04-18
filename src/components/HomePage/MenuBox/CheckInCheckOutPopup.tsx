5e8a64f8ac1fe28f45627c3224e008808b3a971f
stormnoopper <saroot.nae@gmail.com>
2024-04-18 16:28:39 +0700

    Optimize

--- src/components/HomePage/MenuBox/CheckInCheckOutPopup.tsx
+++ src/components/HomePage/MenuBox/CheckInCheckOutPopup.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -7,71 +7,93 @@ import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { toast } from 'sonner';
 
 export const CheckInCheckOutPopup: React.FC<{
-  menuCheckInDate:Dayjs
-  menuCheckOutDate:Dayjs
-  onClose: () => void; 
-  onChange1: (values: any) => void ; 
-  onChange2: (values: any) => void }> = ({ onClose, onChange1, onChange2 ,menuCheckInDate,menuCheckOutDate }) => {
-
-  const [isSubmittedCheckIn, setIsSubmittedCheckIn] = useState(false);
-  const [isSubmittedCheckOut, setIsSubmittedCheckOut] = useState(false);
+  menuCheckInDate: Dayjs;
+  menuCheckOutDate: Dayjs;
+  onClose: () => void;
+  onChange1: (values: any) => void;
+  onChange2: (values: any) => void;
+}> = ({ onClose, onChange1, onChange2, menuCheckInDate, menuCheckOutDate }) => {
   const [checkInDate, setCheckInDate] = useState(menuCheckInDate);
   const [checkOutDate, setCheckOutDate] = useState(menuCheckOutDate);
 
-  var relativeTime = require('dayjs/plugin/relativeTime')
-  dayjs.extend(relativeTime)
+  var useHandleCheckInDateChange = false;
+  var useHandleCheckOutDateChange = false;
 
-  const checkCheckInDate = (newDateIn:any) =>{
-    var x = (newDateIn.fromNow()).substr(-3);
-    if(x==="ago"){
-      toast.error("Wrong Date");
-    }
-    else{
-      setIsSubmittedCheckIn(true)
-      setCheckInDate(newDateIn)
-      onChange1(newDateIn)
-    }
+  const setDefault = () =>{
+    useHandleCheckInDateChange = false;
+    useHandleCheckOutDateChange = false;
   }
 
-  const checkCheckOutDate = (newDateOut:any) =>{
-    var y = (newDateOut.from(checkInDate)).substr(-3);
+  const relativeTime = require('dayjs/plugin/relativeTime');
+  dayjs.extend(relativeTime);
 
-    if(y==="ago"){
-      toast.error("Wrong Date");
-    }
-    else{
-      if(isSubmittedCheckIn===false){
-        toast.error("Please Insert Check-In Date");
+  const checkDateValidity = (newDateIn: any, newDateOut: any) => {
+    var checkInAgo = newDateIn.fromNow().substr(-3);
+    var checkOutAgo = newDateOut.from(newDateIn).substr(-3);
+    
+    if(useHandleCheckInDateChange && useHandleCheckOutDateChange){
+      if (checkOutAgo === 'ago' || checkInAgo === 'ago') {
+        toast.error('Wrong Date');
+        setDefault;
+        return false;
       }
-      else{
-        setIsSubmittedCheckOut(true)
-        setCheckOutDate(newDateOut)
-        onChange2(newDateOut)
-        onClose();
-      }
+      setDefault;
+      return true;
     }
-  }
+    else if(useHandleCheckInDateChange){
+      if (checkInAgo === 'ago'){
+        toast.error('Wrong Date');
+        setDefault;
+        return false;
+      }
+      setDefault;
+      return true;
+    }
+    else if(useHandleCheckOutDateChange){
+      if (checkOutAgo === 'ago') {
+        toast.error('Wrong Date');
+        setDefault;
+        return false;
+      }
+      setDefault;
+      return true;
+    }
+  };
 
-  const closeCheck = (newDateIn:any,newDateOut:any) =>{
-    var z = (newDateOut.from(newDateIn)).substr(-3);
-    console.log(z);
-    if(z==="ago"){
-      toast.error("Wrong Date");
+  const handleCheckInDateChange = (newDateIn: any) => {
+    useHandleCheckInDateChange = true;
+    useHandleCheckOutDateChange = false;
+    if (checkDateValidity(newDateIn, checkOutDate)) {
+      setCheckInDate(newDateIn);
+      onChange1(newDateIn);
     }
-    else{
-      setIsSubmittedCheckIn(true)
-      setCheckInDate(newDateIn)
-      onChange1(newDateIn)
+  };
+
+  const handleCheckOutDateChange = (newDateOut: any) => {
+    useHandleCheckInDateChange = false;
+    useHandleCheckOutDateChange = true;
+    if (checkDateValidity(checkInDate, newDateOut)) {
+      setCheckOutDate(newDateOut);
+      onChange2(newDateOut);
       onClose();
     }
-  }
+  };
+
+  const handleClose = () => {
+    useHandleCheckInDateChange = true;
+    useHandleCheckOutDateChange = true;
+    if (checkDateValidity(checkInDate, checkOutDate)) {
+      onChange1(checkInDate);
+      onClose();
+    }
+  };
 
   return (
     <div className="inset-20 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg shadow-lg p-7">
         <button
           className="absolute top-2 right-2 text-blue-500 hover:text-[#15439C] focus:outline-none transition-all duration-200"
-          onClick={()=>closeCheck(checkInDate,checkOutDate)}
+          onClick={handleClose}
         >
           <svg
             className="h-6 w-6l"
@@ -80,31 +102,24 @@ export const CheckInCheckOutPopup: React.FC<{
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
-        
+
         <div>
-        <h2 className="text-xl font-bold mb-2">Check-In&Check-Out Date</h2>
-        <div className='flex flex-row p-1'>
-          <div className='border'>
+          <h2 className="text-xl font-bold mb-2">Check-In&Check-Out Date</h2>
+          <div className="flex flex-row p-1">
+            <div className="border">
               <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateCalendar value={checkInDate}
-                  onChange={(newDateIn)=>checkCheckInDate(newDateIn)}/>
+                <DateCalendar value={checkInDate} onChange={handleCheckInDateChange} />
               </LocalizationProvider>
             </div>
-            <div className='border'>
+            <div className="border">
               <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateCalendar value={checkOutDate}
-                  onChange={(newDateOut)=>checkCheckOutDate(newDateOut)}/>
+                <DateCalendar value={checkOutDate} onChange={handleCheckOutDateChange} />
               </LocalizationProvider>
             </div>
-        </div>
+          </div>
         </div>
       </div>
     </div>
