698a7ec945f91319876869a5a92f14d8881ec03c
Prushyapong <prushyapong@Prushyapongs-MacBook-Pro.local>
2024-04-24 22:51:41 +0700

    update type

--- @types/type.ts
+++ @types/type.ts
@@ -1,44 +1,44 @@
 import { User } from "next-auth";
 
-
-export interface HotelProps{
-    id:string;
-    name:string;
-    address:string,
-    telephone:string,
-    price:number,
-    tag:string[],
-    review:ReviewProps[],
-    pic:string[],
-    rating:number,
+export interface HotelProps {
+  id: string;
+  name: string;
+  address: string;
+  telephone: string;
+  price: number;
+  tag: string[];
+  review: ReviewProps[];
+  pic: string[];
+  rating: number;
 }
 
-export interface AppointmnetProps{
-    _id:string;
-    user:string;
-    hotel:string;
-    startDate:Date;
-    endDate:Date;
-    status:string;
-    people:number;
-    createdAt:Date;
+export interface AppointmnetProps {
+  _id: string;
+  user: string;
+  hotel: string;
+  startDate: Date;
+  endDate: Date;
+  status: string;
+  people: number;
+  promotion: string;
+  createdAt: Date;
 }
 
-export interface PromotionProps{
-    _id:string;
-    name:string;
-    description:string;
-    startDate:Date;
-    endDate:Date;
-    discount:number;
-    hotel:HotelProps;
-    pic:string;
-    coupon:string;
+export interface PromotionProps {
+  _id: string;
+  name: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  discount: number;
+  hotel: HotelProps;
+  pic: string;
+  coupon: string;
 }
 
-export interface ReviewProps{
-    rating:Number;
-    reviewText:String;
-    user:User;
-    _id:string;
+export interface ReviewProps {
+  rating: Number;
+  reviewText: String;
+  user: User;
+  _id: string;
 }
