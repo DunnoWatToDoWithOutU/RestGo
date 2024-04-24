736bacf885cd94978fd2096179f6c6745e9d2e48
Prushyapong <prushyapong@Prushyapongs-MacBook-Pro.local>
2024-04-24 14:06:38 +0700

    fix url

--- src/libs/getHotel.ts
+++ src/libs/getHotel.ts
@@ -1,11 +1,14 @@
 export default async function getHotel(id: string) {
-  const response = await fetch(`http://localhost:3000/api/v2/hotels/${id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
+  const response = await fetch(
+    `https://rest-go.vercel.app/api/v2/hotels/${id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }
+  );
   if (!response.ok) {
     console.log(response.status);
     throw new Error("Error failed to fetch");
