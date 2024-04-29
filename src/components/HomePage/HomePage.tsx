f240146757dc8b7786ec6692fe54afced7b3fe49
Chokdee-Sigazen <choikdee50@gmail.com>
2024-04-30 03:55:27 +0700

    home page can searh

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -10,13 +10,18 @@ import { HotelProps } from "../../../@types/type";
 
 export function HomePage() {
   const [hotelData, setHotelData] = useState<HotelProps[]>([]);
+
+  const [search, setSearch] = useState("");
   const getHotelData = async () => {
     const hotels = await getHotels();
-    setHotelData(hotels);
+    const results = hotels.filter((hotel: HotelProps) =>
+      hotel.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setHotelData(results);
   };
   useEffect(() => {
     getHotelData();
-  }, []);
+  }, [search]);
 
   return (
     <div>
@@ -24,9 +29,9 @@ export function HomePage() {
         <p className=" text-3xl font-semibold text-white shadow-lg">
           Welcome to REST GO. We will get you to rest in peace.
         </p>
-        <SearchBar></SearchBar>
+        <SearchBar setSearch={setSearch}></SearchBar>
         <MenuBox></MenuBox>
-        <HotelList HotelData={hotelData} checker = {true}></HotelList>
+        <HotelList HotelData={hotelData} checker={true}></HotelList>
       </div>
       <div className=" overflow-hidden h-96 w-full bg-black z-0 absolute left-0 top-0">
         <div className="absolute inset-0 bg-black opacity-15 z-10"></div>
--- src/components/HomePage/HotelList.tsx
+++ src/components/HomePage/HotelList.tsx
@@ -1,10 +1,12 @@
 import { HotelProps } from "../../../@types/type";
 import { HotelCard } from "./HotelCard";
 
-export function HotelList(props: { HotelData: HotelProps[], checker : boolean }) {
-
-  if (props.HotelData.length == 0 && props.checker){
-    return(
+export function HotelList(props: {
+  HotelData: HotelProps[];
+  checker: boolean;
+}) {
+  if (props.HotelData.length == 0 && props.checker) {
+    return (
       <p className="text-center text-xl font-bold m-20">Hotels loading...</p>
     );
   }
