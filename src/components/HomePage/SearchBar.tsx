16fcc8315b3737aa898bf913662a4882502a9cb3
VizKey01 <micmin0101@gmail.com>
2024-04-30 22:41:19 +0700

    fix search bar

--- src/components/HomePage/SearchBar.tsx
+++ src/components/HomePage/SearchBar.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export function SearchBar(props: { setSearch: (search: string) => void }) {
   return (
-    <div className="relative flex items-center">
+    <div className="relative flex items-center my-7 mx-auto justify-center">
       <input
         onChange={(event) => {
           props.setSearch(event.target.value);
         }}
         id="search-input"
-        className="shadow-xl w-[30rem] h-10 p-2 px-4 focus:outline-primary_dark focus:border-[3px] my-7 mx-auto bg-white border-primary border-2 rounded-full"
+        className="shadow-xl w-[30rem] h-10 p-2 px-4 focus:outline-primary_dark focus:border-[3px] bg-white border-primary border-2 rounded-full"
         placeholder="Search hotel"
       />
-      <button className="absolute top-1/2 transform -translate-y-1/2 right-4 py-2">
+      <button className="absolute right-2 top-2">
         <Image
           src={`/icon/searchIcon.png`}
           alt="search Icon"
