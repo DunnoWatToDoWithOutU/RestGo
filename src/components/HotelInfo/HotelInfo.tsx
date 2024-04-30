c94a9afce8feceaf64d3b6d1680e4913e8058e70
Chokdee-Sigazen <choikdee50@gmail.com>
2024-04-30 07:30:59 +0700

    add responsive hotel-info

--- src/components/HotelInfo/HotelInfo.tsx
+++ src/components/HotelInfo/HotelInfo.tsx
@@ -11,6 +11,7 @@ import { Rating } from "@mui/material";
 import createReviews from "@/libs/createReviews";
 import { authOptions } from "@/libs/authOptions";
 import { AddPeoplePopup } from "../HomePage/MenuBox/AddPeoplePopup";
+import { Carousel } from "flowbite-react";
 
 export function HotelInfo(props: {
   hotel: HotelProps;
@@ -101,17 +102,17 @@ export function HotelInfo(props: {
     );
 
   return (
-    <div className=" px-[10%] mt-10   text-[#15439C]">
+    <div className=" md:px-[10%] px-[3%] mt-10   text-[#15439C]">
       <ImageHotel pic={props.hotel.pic} id={props.hotel.id}></ImageHotel>
-      <div className="  my-10 flex w-full space-x-4">
-        <div className="w-2/3 space-y-4 ">
+      <div className="  my-10 md:flex w-full md:space-x-4">
+        <div className="md:w-2/3 w-full space-y-4 ">
           <BasicInfo {...props.hotel}></BasicInfo>
           <div className="bg-white  py-4 w-full border-[#15439C] border-[3px] rounded-2xl p-2 px-4">
             <div className="flex justify-between items-center">
-              <p className="text-3xl font-semibold ml-2">
+              <p className="md:text-3xl text-xl font-semibold ml-2">
                 Reviews ({props.hotel.review.length})
               </p>
-              <div className="flex space-x-2 text-lg items-center">
+              <div className="flex space-x-2 md:text-lg text-sm items-center">
                 <p className=" font-semibold">
                   {Math.round(sumReview * 100) / 100}{" "}
                 </p>
@@ -123,7 +124,7 @@ export function HotelInfo(props: {
                 ></Rating>
               </div>
             </div>
-            <div className="w-full h-[0.125rem] bg-[#15439C] my-5"></div>
+            <div className="w-full h-[0.125rem] bg-[#15439C] md:my-5 my-3"></div>
             <div
               ref={chatMessagesRef}
               className="space-y-3 w-full h-[17rem]  overflow-y-scroll overflow-x-hidden no-scrollbar "
@@ -141,11 +142,11 @@ export function HotelInfo(props: {
             ></InputPanel>
           </div>
         </div>
-        <div className="w-1/3 ">
+        <div className="md:w-1/3 w-full mt-3 md:mt-0 ">
           <div className="w-full h-full bg-white border-[3px] text-[#15439C] rounded-lg p-4 border-[#15439C]">
-            <p className=" text-end text-2xl font-bold">
+            <p className=" text-end text-lg md:text-2xl font-bold">
               Total :{" "}
-              <span className="text-[2.5rem]">
+              <span className="md:text-[2.5rem] text-[1.20rem]">
                 {bookingday == -1
                   ? `Error Booking`
                   : `${
@@ -153,15 +154,15 @@ export function HotelInfo(props: {
                         100) *
                       peopleValues.rooms
                     }฿ `}
-                <p className="text-xl">{` ${peopleValues.rooms} rooms ${
-                  discount > 0 ? ` (discount ${discount}%)` : ``
-                }`}</p>
+                <p className="md:text-xl text-lg">{` ${
+                  peopleValues.rooms
+                } rooms ${discount > 0 ? ` (discount ${discount}%)` : ``}`}</p>
               </span>
             </p>
             <div className="w-full h-[0.125rem] bg-[#15439C] my-5"></div>
-            <p className="  text-2xl font-bold">
+            <p className="  text-lg md:text-2xl font-bold">
               Total :{" "}
-              <span className="text-4xl">
+              <span className="md:text-4xl text-2xl">
                 {(props.hotel.price * (100 - discount)) / 100} ฿{" "}
               </span>{" "}
               <span className="text-lg font-normal">
@@ -172,7 +173,7 @@ export function HotelInfo(props: {
               </span>
             </p>
             <div className="w-full py-2  border-2 border-[#15439C] px-5 relative mt-10 rounded-lg text-center">
-              <p className="text-2xl font-bold">Data Reserve :</p>
+              <p className="md:text-2xl font-bold">Data Reserve :</p>
               <div className="flex mt-4 space-x-2 justify-start items-center">
                 <p className=" text-lg min-w-20 font-bold">From : </p>
 
@@ -271,61 +272,66 @@ export function HotelInfo(props: {
 
 function ImageHotel(props: { pic: string[]; id: string }) {
   return (
-    <div className="flex h-80  w-full space-x-4">
-      <Image
-        alt="hotel"
-        src={`/img/hotel/${props.id}/${props.pic[0]}`}
-        className=" w-2/5 h-full bg-zinc-600 rounded-lg bg-cover bg-center"
-        width={500}
-        height={500}
-      ></Image>
-      <div className=" w-3/5 h-full  space-y-4">
-        <div className="h-[9.5rem] relative w-full  space-x-4 flex">
-          <Image
-            alt="hotel"
-            src={`/img/hotel/${props.id}/${props.pic[1]}`}
-            className="w-1/3 h-full  rounded-md bg-cover bg-center"
-            width={500}
-            height={500}
-          ></Image>
-          <Image
-            alt="hotel"
-            src={`/img/hotel/${props.id}/${props.pic[2]}`}
-            className="w-1/3 h-full  rounded-md bg-cover bg-center"
-            width={500}
-            height={500}
-          ></Image>
-          <Image
-            alt="hotel"
-            src={`/img/hotel/${props.id}/${props.pic[3]}`}
-            className="w-1/3 h-full  rounded-md bg-cover bg-center"
-            width={500}
-            height={500}
-          ></Image>
+    <div className="w-full">
+      <div className=" md:flex hidden h-80  w-full space-x-4">
+        <Image
+          alt="hotel"
+          src={`/img/hotel/${props.id}/${props.pic[0]}`}
+          className=" w-2/5 h-full bg-zinc-600 rounded-lg bg-cover bg-center"
+          width={500}
+          height={500}
+        ></Image>
+        <div className=" w-3/5 h-full  space-y-4">
+          <div className="h-[9.5rem] relative w-full  space-x-4 flex">
+            <Image
+              alt="hotel"
+              src={`/img/hotel/${props.id}/${props.pic[1]}`}
+              className="w-1/3 h-full  rounded-md bg-cover bg-center"
+              width={500}
+              height={500}
+            ></Image>
+            <Image
+              alt="hotel"
+              src={`/img/hotel/${props.id}/${props.pic[2]}`}
+              className="w-1/3 h-full  rounded-md bg-cover bg-center"
+              width={500}
+              height={500}
+            ></Image>
+            <Image
+              alt="hotel"
+              src={`/img/hotel/${props.id}/${props.pic[3]}`}
+              className="w-1/3 h-full  rounded-md bg-cover bg-center"
+              width={500}
+              height={500}
+            ></Image>
+          </div>
+          <div className="h-[9.5rem] relative w-full  space-x-4 flex">
+            <Image
+              alt="hotel"
+              src={`/img/hotel/${props.id}/${props.pic[4]}`}
+              className="w-1/3 h-full  rounded-md bg-cover bg-center"
+              width={500}
+              height={500}
+            ></Image>
+            <Image
+              alt="hotel"
+              src={`/img/hotel/${props.id}/${props.pic[5]}`}
+              className="w-1/3 h-full  rounded-md bg-cover bg-center"
+              width={500}
+              height={500}
+            ></Image>
+            <Image
+              alt="hotel"
+              src={`/img/hotel/${props.id}/${props.pic[6]}`}
+              className="w-1/3 h-full  rounded-md bg-cover bg-center"
+              width={500}
+              height={500}
+            ></Image>
+          </div>
         </div>
-        <div className="h-[9.5rem] relative w-full  space-x-4 flex">
-          <Image
-            alt="hotel"
-            src={`/img/hotel/${props.id}/${props.pic[4]}`}
-            className="w-1/3 h-full  rounded-md bg-cover bg-center"
-            width={500}
-            height={500}
-          ></Image>
-          <Image
-            alt="hotel"
-            src={`/img/hotel/${props.id}/${props.pic[5]}`}
-            className="w-1/3 h-full  rounded-md bg-cover bg-center"
-            width={500}
-            height={500}
-          ></Image>
-          <Image
-            alt="hotel"
-            src={`/img/hotel/${props.id}/${props.pic[6]}`}
-            className="w-1/3 h-full  rounded-md bg-cover bg-center"
-            width={500}
-            height={500}
-          ></Image>
-        </div>
+      </div>
+      <div className="md:hidden w-full">
+        <CarouselPic id={props.id} pic={props.pic}></CarouselPic>
       </div>
     </div>
   );
@@ -334,23 +340,28 @@ function ImageHotel(props: { pic: string[]; id: string }) {
 function BasicInfo(props: HotelProps) {
   return (
     <div className="bg-white py-4 w-full border-[#15439C] border-[3px] rounded-2xl p-2 px-4">
-      <p className="text-3xl font-semibold ml-2">{props.name}</p>
-      <div className="w-full h-[0.125rem] bg-[#15439C] my-5"></div>
-      <div className="pl-7 space-y-3">
-        <p className="text-2xl font-semibold">
+      <p className="text=x; md:text-3xl font-semibold ml-2">{props.name}</p>
+      <div className="w-full h-[0.125rem] bg-[#15439C] md:my-5 my-2"></div>
+      <div className="md:pl-7 pl-2   md:space-y-3 space-y-2">
+        <p className="text-lg md:text-2xl font-semibold">
           Location :{" "}
-          <span className="font-normal text-xl">{props.address}</span>
+          <span className="font-normal text-base md:text-xl">
+            {props.address}
+          </span>
         </p>
-        <p className="text-2xl font-semibold">
-          Tel : <span className="font-normal text-xl">{props.telephone}</span>
+        <p className="text-lg md:text-2xl font-semibold">
+          Tel :{" "}
+          <span className="font-normal text-base md:text-xl">
+            {props.telephone}
+          </span>
         </p>
-        <p className="text-2xl font-semibold">
+        <p className="text-lg md:text-2xl font-semibold">
           Tags :
-          <span className="font-normal text-xl">
+          <span className="font-normal text-base md:text-xl">
             {props.tag.map((tag, index) => {
               return (
                 <span key={index} className="mx-1">
-                  {tag}
+                  {tag},
                 </span>
               );
             })}
@@ -375,12 +386,12 @@ function InputPanel(props: {
         onChange={(event) => {
           props.setReview(event.target.value);
         }}
-        className="h-full rounded-md px-3 py-2 w-[70%] focus:outline-none  "
+        className="h-full rounded-md px-3 py-2 md:w-[70%] w-[50%] focus:outline-none  "
         rows={3}
         cols={50}
         placeholder="Write your review here..."
       ></textarea>
-      <div className="h-full w-[20%] bg-white justify-center flex flex-col rounded-md text-center ">
+      <div className="h-full md:w-[20%] w-[40%] bg-white justify-center flex flex-col rounded-md text-center ">
         <p>{props.Rating}</p>
         <Rating
           name="simple-controlled"
@@ -396,6 +407,22 @@ function InputPanel(props: {
         className="h-10 w-10 bg-cover bg-center hover:scale-110 transition-all duration-200 "
         style={{ backgroundImage: `url(/img/sentbutton.png)` }}
       ></button>
+    </div>
+  );
+}
+
+function CarouselPic(props: { id: string; pic: string[] }): JSX.Element {
+  return (
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel leftControl=";" rightControl=";">
+        <img src={`/img/hotel/${props.id}/${props.pic[0]}`} alt="..." />
+        <img src={`/img/hotel/${props.id}/${props.pic[1]}`} alt="..." />
+        <img src={`/img/hotel/${props.id}/${props.pic[2]}`} alt="..." />
+        <img src={`/img/hotel/${props.id}/${props.pic[3]}`} alt="..." />
+        <img src={`/img/hotel/${props.id}/${props.pic[4]}`} alt="..." />
+        <img src={`/img/hotel/${props.id}/${props.pic[5]}`} alt="..." />
+        <img src={`/img/hotel/${props.id}/${props.pic[6]}`} alt="..." />
+      </Carousel>
     </div>
   );
 }
--- src/components/HotelInfo/ReviewCard.tsx
+++ src/components/HotelInfo/ReviewCard.tsx
@@ -4,18 +4,20 @@ import { Rating } from "@mui/material";
 
 export function ReviewCard(props: { review: ReviewProps }) {
   return (
-    <div className="h-20 w-full space-x-3  relative bg-white border-[#15439C] items-center px-3 flex border-2">
+    <div className=" py-3  w-full space-x-3  relative bg-white border-[#15439C] items-center px-3 flex border-2">
       <div className=" h-14 min-w-12 rounded-full ">
         <Avvvatars value={props.review.user.name ?? ""} size={48} />
       </div>
       <div className=" flex flex-col text-wrap  w-full">
-        <p className=" font-bold text-lg">{props.review.user.name}</p>
-        <div className="text-ellipsis whitespace-normal">
+        <p className=" font-bold md:text-lg text-sm w-[60%]">
+          {props.review.user.name}
+        </p>
+        <div className="text-ellipsis md:text-base text-sm w-[50%] whitespace-normal">
           {props.review.reviewText}
         </div>
       </div>
       <div className="h-10 w-32 absolute right-2 top-5 flex items-center">
-        <p className="text-lg font-bold mr-1">
+        <p className="md:text-lg text-sm font-bold mr-1">
           {props.review.rating.toString()}
         </p>
         <Rating
