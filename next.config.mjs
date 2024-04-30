57454c1fe791989ada5af9ae4853f071749dab50
Chokdee-Sigazen <choikdee50@gmail.com>
2024-04-30 19:00:45 +0700

    fix build(again)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },};
 
 export default nextConfig;
