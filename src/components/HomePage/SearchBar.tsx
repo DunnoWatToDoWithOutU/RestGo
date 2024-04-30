3397fad9ba4f63abaa5ac31bf2cb7c972a02e647
VizKey01 <micmin0101@gmail.com>
2024-04-30 23:01:40 +0700

    fix main searchbar

--- src/components/HomePage/SearchBar.tsx
+++ src/components/HomePage/SearchBar.tsx
@@ -11,14 +11,16 @@ export function SearchBar(props: { setSearch: (search: string) => void }) {
         className="shadow-xl w-[30rem] h-10 p-2 px-4 focus:outline-primary_dark focus:border-[3px] bg-white border-primary border-2 rounded-full"
         placeholder="Search hotel"
       />
-      <button className="absolute right-2 top-2">
-        <Image
-          src={`/icon/searchIcon.png`}
-          alt="search Icon"
-          width={20}
-          height={20}
-        />
-      </button>
+      <div className="grid content-center absolute right-0 left-[27rem]">
+        <button className="">
+          <Image
+            src={`/icon/searchIcon.png`}
+            alt="search Icon"
+            width={20}
+            height={20}
+          />
+        </button>
+      </div>
     </div>
   );
 }
